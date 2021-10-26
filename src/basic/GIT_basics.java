a166bd17560d182e77b6d33c5f30dfb9fd3610ec
agamsachdev <75251295+agamsachdev@users.noreply.github.com>
2021-10-27 02:29:43 +0530

    First version of basic code

---
+++ src/basic/GIT_basics.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class GIT_basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First GIT repository");
+
+	}
+
+}
